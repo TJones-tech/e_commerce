db2c409c934de49940bfcebce23d7c01cb435893
TJonesJr13 <t.jones50.tj@gmail.com>
2022-06-17 16:59:26 -0500

    First commit

---
+++ src/components/Products.jsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { popularProducts } from "../data";
+import Product from "./Product";
+
+
+const Container = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] =useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try{
+        const res = await axios.get("http://localhost:7000/api/products");
+        console.log(res);
+      } catch (err){
+
+      }
+    };
+    getProducts()
+  }, [cat]);
+
+  return (
+    <Container>
+        {popularProducts.map((item) => (
+            <Product item={item} key={item.id} />
+        ))}
+    </Container>
+  )
+}
+
+export default Products;
